c0867ce25bec6f163502a43cd76da192fb5acb5f
Luis Antónimo <luisantonimo@gmail.com>
2022-07-12 08:40:17 +0200

    ok

--- Program.cs
+++ Program.cs
@@ -1,3 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+namespace T04Ejercicio4
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Diga un número (A):");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Diga otro número (B):");
+            int b = Convert.ToInt32(Console.ReadLine());
+            if (a > b)
+            {
+                Console.WriteLine("El número A es mayor que B.");
+            }
+            else
+            {
+                if (a == b)
+                {
+                    Console.WriteLine("El número A es igual a B.");
+                }
+                else
+                {
+                    Console.WriteLine("El número B es mayor que A.");
+                }
+            }
+        }
+    }
+}
